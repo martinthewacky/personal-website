99d7c415ff56c37b57131d1da69bde11fae54e5a
Martin Matovu <96186332+martinthewacky@users.noreply.github.com>
2022-11-09 17:23:34 +0300

    Add profile card to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import ProfileCard from "components/ProfileCard";
 
 import styles from "../styles/Home.module.css";
 
@@ -12,7 +13,9 @@ function HomePage() {
       </Head>
       <Header />
       <main className={styles.main}>
-        <div className={styles.mainContentFlow}></div>
+        <div className={styles.mainContentFlow}>
+          <ProfileCard />
+        </div>
       </main>
     </div>
   );
