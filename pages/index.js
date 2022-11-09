0299baeafbaa60fb28d6948d66bd8f3a2cccd6c9
Martin Matovu <96186332+martinthewacky@users.noreply.github.com>
2022-11-09 13:54:57 +0300

    Edit main section styles

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,13 @@ import styles from "../styles/Home.module.css";
 
 function HomePage() {
   return (
-    <div>
+    <div className={styles.homePage}>
       <Head>
         <title>Martin Matovu's Home</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Header />
+      <main className={styles.main}></main>
     </div>
   );
 }
