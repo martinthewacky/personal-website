859353500a6dd3cb71a620085609e7cdc677d376
Martin Matovu <96186332+martinthewacky@users.noreply.github.com>
2022-11-09 11:06:19 +0300

    Make basic structure of Header component

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,3 +1,27 @@
-const Header = () => {};
+import Link from "next/link";
+
+import { AtSymbolIcon } from "@heroicons/react/24/outline/AtSymbolIcon";
+import { MoonIcon } from "@heroicons/react/20/solid/MoonIcon";
+import { Bars2Icon } from "@heroicons/react/20/solid/Bars2Icon";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <Link>
+          <AtSymbolIcon />
+        </Link>
+      </div>
+      <div>
+        <button>
+          <MoonIcon />
+        </button>
+        <button>
+          <Bars2Icon />
+        </button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
