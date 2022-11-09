c48b2cb9dbc354dc979777c3be79cdef41bd8576
Martin Matovu <96186332+martinthewacky@users.noreply.github.com>
2022-11-09 18:14:05 +0300

    Place working icons in header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import { AtSymbolIcon } from "@heroicons/react/24/outline";
+import { MoonIcon } from "@heroicons/react/24/outline";
+import { Bars2Icon } from "@heroicons/react/24/outline";
 
 import styles from "./Header.module.css";
 
@@ -6,11 +9,17 @@ export default function Header() {
   return (
     <header className={styles.head}>
       <div className={styles.headerSectionLeft}>
-        <Link href="/">@</Link>
+        <Link href="/">
+          <AtSymbolIcon className={styles.headerSectionLogo} />
+        </Link>
       </div>
       <div className={styles.headerSectionRight}>
-        <button className={styles.headerButton}>Theme</button>
-        <button className={styles.headerButton}>Menu</button>
+        <button className={styles.headerButton}>
+          <MoonIcon className={styles.headerIcon} />
+        </button>
+        <button className={styles.headerButton}>
+          <Bars2Icon className={styles.headerIcon} />
+        </button>
       </div>
     </header>
   );
