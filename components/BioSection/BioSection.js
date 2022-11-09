f6aee1adf0c2bb36b4aff54d7c5ede185069a3c9
Martin Matovu <96186332+martinthewacky@users.noreply.github.com>
2022-11-09 19:22:03 +0300

    Add card component to bio section

--- components/BioSection/BioSection.js
+++ components/BioSection/BioSection.js
@@ -1,7 +1,13 @@
+import Card from "components/Card";
+import styles from "./BioSection.module.css";
+
 export default function BioSection() {
   return (
     <section>
       <h2>BIO</h2>
+      <Card>
+        <div></div>
+      </Card>
     </section>
   );
 }
