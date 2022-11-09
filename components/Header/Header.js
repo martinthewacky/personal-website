b2cf1311f9ed4654e7f75c62893010637b6088e9
Martin Matovu <96186332+martinthewacky@users.noreply.github.com>
2022-11-09 12:13:24 +0300

    Remove heroicons imports which cause errors

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,24 +1,14 @@
 import Link from "next/link";
 
-import { AtSymbolIcon } from "@heroicons/react/24/outline/AtSymbolIcon";
-import { MoonIcon } from "@heroicons/react/20/solid/MoonIcon";
-import { Bars2Icon } from "@heroicons/react/20/solid/Bars2Icon";
-
 export default function Header() {
   return (
     <header>
       <div>
-        <Link href="/">
-          <AtSymbolIcon />
-        </Link>
+        <Link href="/">@</Link>
       </div>
       <div>
-        <button>
-          <MoonIcon />
-        </button>
-        <button>
-          <Bars2Icon />
-        </button>
+        <button>Toggle Theme</button>
+        <button>Menu</button>
       </div>
     </header>
   );
