eae3d8269e051807db9e598a7210c7b0b8500dcd
Martin Matovu <96186332+martinthewacky@users.noreply.github.com>
2022-11-09 10:11:28 +0300

    Add title and meta viewport to head

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Head from "next/head";
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div>
+      <Head>
+        <title>Martin Matovu's Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+    </div>
+  );
 }
 
 export default HomePage;
